8210f918b23818700b4c4329bc6249f47f0c0c72
Honguito <eduardolalojami@gmail.com>
2021-09-09 20:13:07 -0500

    POO ejercicios

--- lab1/main.js
+++ lab1/main.js
@@ -40,3 +40,109 @@ const juanita = new Student(
     ["Curso de ingles",
     "Curso de Español"]
 );
+
+// Prototipos con sintaxis clases
+class Student2 {
+    constructor({
+      name,
+      cursosAprobados = [],
+      age,
+      email,
+    }) {
+      this.name = name;
+      this.email = email;
+      this.age = age;
+      this.cursosAprobados = cursosAprobados;
+    }
+  
+    aprobarCurso(nuevoCursito) {
+      this.cursosAprobados.push(nuevoCursito);
+    }
+  }
+  
+  const miguelito = new Student2({
+    name: "Miguel",
+    age: 28,
+    email: "miguelito@platzi.com",
+  });
+
+  // Crear estudiantes on objetos literales
+
+const juan1 = {
+    name: "Edu",
+    username: "honquito",
+    points: 100,
+    socialMedia: {
+        twitter: "BryanJami",
+        instagram: "Lalo",
+        facebook: underfined,
+    },
+    approvedCourses: [
+        "Curso de ingles",
+        "Curso de Mate",
+    ],
+    learningPath: [
+        {
+            name: "Escuela de DW",
+            courses: [
+                "Curso de ingles",
+                "Curso de Mate",
+                "Curso de Quimica",
+            ]
+        }
+    ]
+}
+
+class LearningPath {
+
+}
+
+const escuelaWeb = new LearningPath();
+const escuelaData = new LearningPath();
+const escuelaVgs = new LearningPath();
+
+class Student {
+  constructor({
+    name,
+    email,
+    username,
+    twitter = undefined,
+    instagram = undefined,
+    facebook = undefined,
+    approvedCourses = [],
+    learningPaths = [],
+  }) {
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.socialMedia = {
+      twitter,
+      instagram,
+      facebook,
+    };
+    this.approvedCourses = approvedCourses;
+    this.learningPaths = learningPaths;
+  }
+}
+
+const juan2 = new Student({
+  name: "JuanDC",
+  username: "juandc",
+  email: "juanito@juanito.com",
+  twitter: "fjuandc",
+  learningPaths: [
+    escuelaWeb,
+    escuelaVgs,
+  ],
+});
+
+const miguelito2 = new Student({
+  name: "Miguelito",
+  username: "migelitofeliz",
+  email: "miguelito@juanito.com",
+  instagram: "migelito_feliz",
+  learningPaths: [
+    escuelaWeb,
+    escuelaData,
+  ],
+});
